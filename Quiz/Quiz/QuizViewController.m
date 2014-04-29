46ddd2c3097f99e6e67e225ff31e8b1fb010a1c2
fengchaoyi <fengchaoyi@Neoman-iMac.local>
2014-04-29 14:00:45 +0800

    4.29afternoon

--- Quiz/Quiz/QuizViewController.m
+++ Quiz/Quiz/QuizViewController.m
@@ -28,12 +28,32 @@
 
 //数据源
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return 1;
+    return 5;
 }
 
 -(UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ListItem"];
+    UILabel *label = (UILabel*)[cell viewWithTag:1000];
+    if (indexPath.row==0){
+        label.text=@"First thing to do";
+    }
+    else if (indexPath.row==1){
+        label.text=@"Second thing to do";
+    }
     return cell;
 }
+
+//Delegate 代理
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{   //此处应该是didSelect
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if (cell.accessoryType == UITableViewCellAccessoryNone){
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else{
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 
 @end
